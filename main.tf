0523865071063eb25b3edb8cb9e6574e4c4db412
manu <manu@local.com>
2022-05-22 20:11:03 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -2,14 +2,6 @@
 #  source = "git::https://github.com/ashwinreddy9966/tf-module-vpc.git?ref=main"
 #}
 
-module "vpc" {
-  source           = "./vendor/modules/vpc" # we made this as the source as Terrafile places the remote source module and place here
-  VPC_CIDR         = var.VPC_CIDR
-  ENV              = var.ENV
-  AZ               = var.AZ
-  DEFAULT_VPC_ID   = var.DEFAULT_VPC_ID
-  DEFAULT_VPC_CIDR = var.DEFAULT_VPC_CIDR
-  DEFAULT_VPC_RT   = var.DEFAULT_VPC_RT
-  PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
-  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
+module "mongodb" {
+  source           = "./vendor/modules/mongodb" # we made this as the source as Terrafile places the remote source module and place here
 }
