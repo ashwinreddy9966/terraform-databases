9749b66ec1bba503655d63671839327b6bf83973
manu <manu@local.com>
2022-05-24 00:05:31 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -1,19 +1,23 @@
 module "mongodb" {
   source           = "./vendor/modules/mongodb" # we made this as the source as Terrafile places the remote source module and place here
   ENV              = var.ENV
+  WORKSTATION_IP   = var.WORKSTATION_IP
 }
 
 module "redis" {
   source           = "./vendor/modules/redis" # we made this as the source as Terrafile places the remote source module and place here
   ENV              = var.ENV
+  WORKSTATION_IP   = var.WORKSTATION_IP
 }
 
 module "mysql" {
   source           = "./vendor/modules/mysql" # we made this as the source as Terrafile places the remote source module and place here
   ENV              = var.ENV
+  WORKSTATION_IP   = var.WORKSTATION_IP
 }
 
 module "rabbitmq" {
   source           = "./vendor/modules/rabbitmq" # we made this as the source as Terrafile places the remote source module and place here
   ENV              = var.ENV
+  WORKSTATION_IP   = var.WORKSTATION_IP
 }
