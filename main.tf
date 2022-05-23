c8e2311033f3d3fded02d86baf089b8deddb8acf
manu <manu@local.com>
2022-05-23 19:59:24 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -6,3 +6,8 @@ module "mongodb" {
   source           = "./vendor/modules/mongodb" # we made this as the source as Terrafile places the remote source module and place here
   ENV              = var.ENV
 }
+
+module "redis" {
+  source           = "./vendor/modules/redis" # we made this as the source as Terrafile places the remote source module and place here
+  ENV              = var.ENV
+}
