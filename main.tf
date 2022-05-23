35b2ae936ae60ad3d194510d0064088ad80b04fa
manu <manu@local.com>
2022-05-23 21:07:39 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-#module "vpc" {
-#  source = "git::https://github.com/ashwinreddy9966/tf-module-vpc.git?ref=main"
-#}
-
 module "mongodb" {
   source           = "./vendor/modules/mongodb" # we made this as the source as Terrafile places the remote source module and place here
   ENV              = var.ENV
