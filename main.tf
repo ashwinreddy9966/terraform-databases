56d9c52205f04a5e227b93357460bab2e4743c2b
manu <manu@local.com>
2022-05-24 10:59:54 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -11,9 +11,13 @@ module "redis" {
 }
 
 module "mysql" {
-  source           = "./vendor/modules/mysql" # we made this as the source as Terrafile places the remote source module and place here
-  ENV              = var.ENV
-  WORKSTATION_IP   = var.WORKSTATION_IP
+  source             = "./vendor/modules/mysql" # we made this as the source as Terrafile places the remote source module and place here
+  ENV                = var.ENV
+  WORKSTATION_IP     = var.WORKSTATION_IP
+  RDS_MYSQL_PORT     = var.RDS_MYSQL_PORT
+  RDS_MYSQL_STORAGE  = var.RDS_MYSQL_STORAGE
+  RDS_ENGINE_VERSION = var.RDS_ENGINE_VERSION
+  RDS_INSTANCE_TYPE  = var.RDS_INSTANCE_TYPE
 }
 
 module "rabbitmq" {
